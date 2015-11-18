a7ad413adbc62744d8f0657d8dce4438dc601fc7
strucoder <zdwloveschina@163.com>
2015-11-18 19:39:01 +0800

    fix gen css  not found

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,12 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./public/css'))
 })
 
+gulp.task('copy', function() {
+  return gulp.src('./front/simditor.css')
+    .pipe(gulp.dest('./public/css'))
+})
 gulp.task('sass-watch', function() {
   return gulp.watch('./front/scss/*.scss', ['sass'])
 });
 
-gulp.task('default', ['sass', 'sass-watch'])
+gulp.task('default', ['copy', 'sass', 'sass-watch'])
