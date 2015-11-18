fdbbb6f96116822368eac76133c61f43781d08a1
strucoder <zdwloveschina@163.com>
2015-11-18 18:42:04 +0800

    finish export logic and frontend logic

--- index.js
+++ index.js
@@ -3,11 +3,92 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const handlebars = require('handlebars')
 const path = require('path');
+const bodyParser = require('body-parser');
 const config = require('./config');
+const fs = require('fs');
+const tools = require('./lib/tools');
+const wkhtml = require('./lib/wkhtml');
+const validType = ['pdf', 'png'];
+const typeMap = {
+  'pdf': 'wkhtmltopdf',
+  'png': 'wkhtmltoimage'
+};
 
+const baseImgsPath = path.join(__dirname, '/images/');
+let tpl = fs.readFileSync(path.join(__dirname,'/tpl/exportTpl.html'), {encoding: 'utf8'});
+let tplCompiled = handlebars.compile(tpl);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+function route(app) {
+  app.post('/gen', function(req, res) {
+    const body = req.body;
+    const htmlVal = body.data;
+    const type = body.type;
+    const command = typeMap[type];
+    if (!command) {
+      res.statusCode = 400;
+      res.end('only suppprt png or pdf type');
+      return;
+    }
+    const retHtml = tplCompiled({
+      html: htmlVal
+    });
+    let options;
+    let filePath;
+    let filename;
+    if (type === 'png') {
+      options = {quality: 100};
+      filename = tools.genUid() + '.png'
+      filePath = path.join(baseImgsPath, filename);
+    } else {
+      options =  {pageSize: 'A4'};
+      filename = tools.genUid() + '.pdf'
+      filePath = path.join(baseImgsPath, filename);
+    }
+    let streamTmp = fs.createWriteStream(filePath);
+    let pipeStream = wkhtml(command, retHtml,options).pipe(streamTmp);
+    pipeStream.on('finish', function() {
+       var retData = {
+        code: 0,
+        type: type,
+        filename: filename
+      };
+      res.send(retData);
+    });
+  });
+
+  app.get('/download', function(req, res) {
+    const query = req.query;
+    const filename = query.filename;
+    const type = query.type;
+    if (validType.indexOf(type) === -1) {
+      res.statusCode = 400;
+      res.end('type Wrong');
+      return;
+    }
+    const filePath = path.join(baseImgsPath, filename);
+    if(!fs.existsSync(filePath)) {
+      res.statusCode = 403;
+      res.end('forbidden')
+      return;
+    }
+    const fileSize = fs.statSync(filePath).size;
+    let readStream = fs.createReadStream(filePath);
+    res.setHeader('Content-type', 'image/' + type);
+    res.setHeader('Content-length', fileSize);
+    res.setHeader('Content-disposition','attachment; filename="' + filename + '"');
+    readStream.pipe(res);
+  })
+}
+
+route(app)
 
 http.createServer(app).listen(config.port, function() {
   console.log('server was running at ', config.port)
-})
+});
--- public/js/editorExt.js
+++ public/js/editorExt.js
@@ -1,10 +1,113 @@
 (function(Simditor, SEINS) {
+  var $exportBox = $('.export-box');
+  var $cover = $('.cover');
+  var $close = $exportBox.find('.header .close');
   var toolbar = SEINS.toolbar;
   var exportTpl = '<li><a href="javascript:;" class="toolbar-item toolbar-item-export"><span>导出</span></a></li>';
   var toolbarUl = toolbar.wrapper.find('ul');
+  var $pngBtn = $exportBox.find('.body .left');
+  var $pdfBtn = $exportBox.find('.body .right');
+  var $waitingBox = $('.waiting-box');
+
+  function simulate(element, eventName) {
+    var options = extend(defaultOptions, arguments[2] || {});
+    var oEvent, eventType = null;
+
+    for (var name in eventMatchers) {
+      if (eventMatchers[name].test(eventName)) {
+        eventType = name;
+        break;
+      }
+    }
+
+    if (!eventType)
+      throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+    if (document.createEvent) {
+      oEvent = document.createEvent(eventType);
+      if (eventType == 'HTMLEvents') {
+        oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+      } else {
+        oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+          options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+          options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+      }
+      element.dispatchEvent(oEvent);
+    } else {
+      options.clientX = options.pointerX;
+      options.clientY = options.pointerY;
+      var evt = document.createEventObject();
+      oEvent = extend(evt, options);
+      element.fireEvent('on' + eventName, oEvent);
+    }
+    return element;
+  }
+
+  function extend(destination, source) {
+    for (var property in source)
+      destination[property] = source[property];
+    return destination;
+  }
+
+  var eventMatchers = {
+    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+  }
+  var defaultOptions = {
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+  }
   $(toolbar._tpl.separator).appendTo(toolbarUl);
   $(exportTpl).appendTo(toolbarUl);
   toolbarUl.find('li .toolbar-item-export').click(function() {
-    console.log('...')
-  })
+    $cover.addClass('show');
+    $exportBox.addClass('show');
+  });
+
+  $close.click(function() {
+    $exportBox.removeClass('show');
+    $cover.removeClass('show');
+  });
+
+  function download(filename, type) {
+    var downloadUrl = '/download?filename=' + filename + '&type=' + type;
+    var aEl = document.createElement('a');
+    aEl.href = downloadUrl;
+    simulate(aEl, 'click');
+    $waitingBox.removeClass('show');
+    $cover.removeClass('show');
+  }
+  $pngBtn.click(function() {
+    $exportBox.removeClass('show');
+    $waitingBox.addClass('show');
+
+    var postData = {
+      data: SEINS.getValue(),
+      type: 'png'
+    }
+
+    $.post('/gen', postData, function(res) {
+      download(res.filename, res.type);
+    });
+  });
+
+  $pdfBtn.click(function() {
+    $exportBox.removeClass('show');
+    $waitingBox.addClass('show');
+    var postData = {
+      data: SEINS.getValue(),
+      type: 'pdf'
+    }
+    $.post('/gen', postData, function(res) {
+      download(res.filename, res.type);
+    });
+  });
+
 })(window.Simditor, window.SEINS)
